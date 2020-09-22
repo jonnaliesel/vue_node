82769e0a84b01b6f0224750672182f7c72bf8811
Jonna <jonnaliesel@gmail.com>
2020-09-22 10:14:33 +0200

    Add while loop for mock data

--- vue-server/index.js
+++ vue-server/index.js
@@ -12,12 +12,14 @@ io.on('connection', (socket) => {
     console.log('connected')
 
     async function sendData() {
-        await new Promise((resolve) => setTimeout(resolve, 1000))
+        while(true) {
+            await new Promise((resolve) => setTimeout(resolve, 1000))
         
-        const data = await getRandomValueArray()
+            const data = await getRandomValueArray()
 
-        console.log('Sending:' +  JSON.stringify(data, null, 2))
-        socket.broadcast.emit('newChartData', data)
+            console.log('Sending:' +  JSON.stringify(data, null, 2))
+            socket.broadcast.emit('newChartData', data)
+        }
     }
 
     sendData()
