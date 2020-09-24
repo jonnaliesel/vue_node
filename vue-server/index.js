671d71849ff116c5e66d1294b22138e1f6fd187d
Jonna <jonnaliesel@gmail.com>
2020-09-24 08:02:28 +0200

    Add speedometer

--- vue-server/index.js
+++ vue-server/index.js
@@ -1,31 +1,26 @@
-const app = require('express')
-const http = require('http').createServer(app)
-const io = require('socket.io')(http)
-
-const port = 5000
+const app = require('express');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 function getRandomValueArray() {
-    return Array.from({ length: 10 }, () => Math.floor(Math.random() * 100 ))
+  return Array.from({ length: 10 }, () => Math.floor(Math.random() * 100));
 }
 
 io.on('connection', (socket) => {
-    console.log('connected')
+  async function sendData() {
+    while (true) {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
 
-    async function sendData() {
-        while(true) {
-            await new Promise((resolve) => setTimeout(resolve, 1000))
-        
-            const data = await getRandomValueArray()
+      const data = await getRandomValueArray();
 
-            console.log('Speedometer value:' +  JSON.stringify(data[0]))
-            socket.broadcast.emit('newChartData', data)
-            socket.broadcast.emit('newSpeedometerValue', data[0])
-        }
+      console.log('Sending: ' + JSON.stringify(data));
+      socket.broadcast.emit('newChartData', data);
+      socket.broadcast.emit('newSpeedometerValue', data[0]);
     }
+  }
+  sendData();
+});
 
-    sendData()
-})
-
-http.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-})
+http.listen(5000, () => {
+  console.log('Server listening on port 5000');
+});
